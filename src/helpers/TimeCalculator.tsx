c1f37f15e29454df2e31ff67f81decbcc12b7757
Злой Босс <alibek.mombekova@mail.ru>
2023-03-16 11:26:08 +0600

    refactor

--- src/helpers/TimeCalculator.tsx
+++ src/helpers/TimeCalculator.tsx
@@ -16,11 +16,24 @@ export class TimeCalculator {
     minute = 0,
     second = 0
   ) {
-    const now: any = new Date();
-    const targetDate: any = new Date(year, month, day, hour, minute, second);
-    console.log(targetDate);
+    const now: Date = new Date();
+    const targetDate: Date = new Date(year, month, day, hour, minute, second);
+    return this.calculateTimeBetween(now, targetDate);
+  }
 
-    let seconds = Math.floor((targetDate - now) / 1000);
+  public static isWeekend(dayOfWeek: number) {
+    return [6, 7].findIndex((day) => day === dayOfWeek) !== -1;
+  }
+
+  public static precentComplete(overallDay: number) {
+    const now: any = new Date();
+    const startDate: any = new Date(2023, 1, 28);
+    const { days: daysPassed } = this.calculateTimeBetween(startDate, now);
+    return Math.floor((daysPassed / overallDay) * 100);
+  }
+
+  public static calculateTimeBetween(startDate: Date, endDate: Date) {
+    let seconds = Math.floor((endDate.getTime() - startDate.getTime()) / 1000);
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
@@ -30,24 +43,5 @@ export class TimeCalculator {
     seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
 
     return { seconds, minutes, hours, days };
-  }
-
-  public static isWeekend(dayOfWeek: number) {
-    return [6, 7].findIndex((day) => day === dayOfWeek) !== -1;
-  }
-
-  public static precentComplete(overallDay: number) {
-    // 67 дней в общем
-    const now: any = new Date();
-    const startDate: any = new Date(2023, 1, 28);
-    console.log(startDate);
-
-    let seconds = Math.floor((now - startDate) / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let daysPassed = Math.floor(hours / 24);
-
-    // вернуть в процентах пройденное количество
-    return Math.floor((daysPassed / overallDay) * 100);
   }
 }
